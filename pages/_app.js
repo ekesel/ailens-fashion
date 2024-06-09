7a3a6887b767a4fad125dc4a65b75f05a49ad1d4
Ekaansh Sahni <ekaansh.sahni@gmail.com>
2024-06-09 12:21:48 +0530

    Font optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.css'
+import { Poppins } from 'next/font/google'
 
 import { useEffect } from "react";
+
+const poppins = Poppins({ subsets: ['latin'],
+  weight: ['200','400', '600', '700', '800', '300'],
+ })
+ 
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
   return (
-    <>
+    <main className={poppins.className}>
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
